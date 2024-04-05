301699abb8339a0f48069f3dc52ee31c41f8d001
GuyFromEkb <guyFromEkb@example.com>
2024-04-05 23:24:36 +0500

    fix(Swagger): обработка файлов для cli (автогенерации свагера)

--- src/user/response/user.response.ts
+++ src/user/response/user.response.ts
@@ -1,5 +1,6 @@
 import { Collection } from "@mikro-orm/core";
 import { Injectable } from "@nestjs/common";
+import { ApiHideProperty } from "@nestjs/swagger";
 import { Exclude } from "class-transformer";
 
 import { UserSessionEntity } from "~auth/entities/user-session.entity";
@@ -7,27 +8,36 @@ import { UserEntity, UserRole } from "~user/entities/user.entity";
 
 @Injectable()
 export class UserResponse implements Partial<UserEntity> {
+  /** user email @example herberSpenser@yahoo.com */
   email: string;
 
-  @Exclude()
-  password: string;
-
+  /** user name @example 'Bob Walter' */
   username: string;
 
-  bio: string;
+  /** user short information @example 'I like turtle' */
+  bio?: string;
 
+  /** user GUID @example b3705ec2-51f0-4baa-a4cb-b1fba6ecca14*/
   id: string;
 
+  /** user role @example USER */
   role: UserRole;
 
+  @ApiHideProperty()
   @Exclude()
   createdAt: Date;
 
+  @ApiHideProperty()
   @Exclude()
   updatedAt: Date;
 
+  @ApiHideProperty()
   @Exclude()
   session?: Collection<UserSessionEntity>;
+
+  @ApiHideProperty()
+  @Exclude()
+  password: string;
 
   constructor(userEntity: Partial<UserEntity>) {
     Object.assign(this, userEntity);
